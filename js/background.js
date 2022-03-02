f1100696f39f09f89cd83f5e42985ade73cef7a7
codenname007 <81899298+codenname007@users.noreply.github.com>
2022-03-02 20:44:09 +0900

    todolist-first-verson

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const body = document.body;
+
+const background = ["배경1.jpg","배경2.jpg","배경3.jpg","배경4.jpg","배경5.jpg","배경6.jpg","배경7.jpg","배경8.jpg","배경9.jpg","배경10.jpg","배경11.jpg","배경12.jpg"];
+
+const randomIndex = Math.floor(Math.random()*background.length);
+
+
+function makeUrl(){
+  return `url(../image/${background[randomIndex]})
+  no-repeat center center`
+}
+
+body.style.background = makeUrl();
